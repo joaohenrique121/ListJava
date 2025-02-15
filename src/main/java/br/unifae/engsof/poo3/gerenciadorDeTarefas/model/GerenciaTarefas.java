4d750e0f92bc44e7d88c285653a4368c9e3cfbbc
joaohenrique121 <simionatojoaohenrique@gmail.com>
2025-02-15 11:33:48 -0300

    metodos gerenciadortarefas

--- src/main/java/br/unifae/engsof/poo3/gerenciadorDeTarefas/model/GerenciaTarefas.java
+++ src/main/java/br/unifae/engsof/poo3/gerenciadorDeTarefas/model/GerenciaTarefas.java
@@ -4,8 +4,9 @@
  */
 package br.unifae.engsof.poo3.gerenciadorDeTarefas.model;
 
-import java.awt.List;
+import java.util.List;
 import java.util.ArrayList;
+
 
 
 
@@ -14,9 +15,24 @@ import java.util.ArrayList;
  * @author android
  */
 public class GerenciaTarefas {
-    private List<Tarefa> Tarefa = new ArrayList<Tarefa>;
-    
-    public Tarefa addTask(){
+    private List<Tarefa> t = new ArrayList<Tarefa>();
+
+    public boolean addTask(Tarefa tarefa){
+        if(!t.contains(tarefa)) return false;
         
+        return t.add(tarefa);
     } 
+    
+    public boolean remove(Tarefa tarefa){
+        if(!t.contains(tarefa)) return false;
+        
+        return t.remove(tarefa);
+    }
+    
+    public List<Tarefa> show(){
+        if(!t.isEmpty())return this.t;
+        
+        return (List<Tarefa>) t.get(0);
+
+    }
 }
